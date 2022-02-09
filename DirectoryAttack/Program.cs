cee55a66e0b33c1fe495a25e871d1141e34c987b
GeorgiyEihler <homosapien777@gmail.com>
2022-02-09 13:58:55 +0600

    Доп задание

---
+++ DirectoryAttack/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Создание дирректорий");
+for (int i = 0; i < 100; i++)
+{
+   new DirectoryInfo($"Directory_{i}").Create();
+}
+Console.WriteLine("создание дирректорийз авершилосб");
+Console.WriteLine("удаление дирректорий");
+
+for (int i = 0; i < 100; i++)
+{
+    if(Directory.Exists($"Directory_{i}"))
+        new DirectoryInfo($"Directory_{i}").Delete(true);
+}
+Console.WriteLine("удаление дирректорийз авершилосб");
+
